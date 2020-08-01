2e6f53f2643d75a9727c400bdaf119db7d8291ea
Anzhelika Vlasenko <68965329+likavlasenko@users.noreply.github.com>
2020-08-01 15:33:49 -0500

    Update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,5 +1,10 @@
 public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello B20!");
+        
+        
+        // I will see this line on Intellije
+        //added it from GitHub
+        
     }
 }
